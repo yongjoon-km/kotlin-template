d3ed82a3bb6d613d3f561c24000c19baebc84064
yongjoon <penubokim@gmail.com>
2022-06-22 00:22:11 +0900

    Change feign client library compatible with spring boot 2.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.3")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
